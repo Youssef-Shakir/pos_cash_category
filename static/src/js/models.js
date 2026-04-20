e3b222f15526591c45da5bad5f5d1441762bfff7
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-20 03:36:24 +0300

    [DEBUG] pos_cash_category: Add console logging for debugging

--- static/src/js/models.js
+++ static/src/js/models.js
@@ -11,36 +11,45 @@ patch(PosStore.prototype, {
         if (this._cashCategoriesLoaded) {
             return;
         }
-        if (this.config.use_cash_categories) {
-            try {
-                this.cashCategories = await jsonrpc("/web/dataset/call_kw/pos.session/get_cash_categories", {
-                    model: "pos.session",
-                    method: "get_cash_categories",
-                    args: [[this.pos_session.id]],
-                    kwargs: {},
-                });
-                this._cashCategoriesLoaded = true;
-            } catch (error) {
-                console.error("Error loading cash categories:", error);
-                this.cashCategories = [];
-            }
-        } else {
+
+        const sessionId = this.pos_session?.id || this.session?.id;
+        console.log("[CashCategory] Loading categories, session:", sessionId);
+
+        try {
+            this.cashCategories = await jsonrpc("/web/dataset/call_kw/pos.session/get_cash_categories", {
+                model: "pos.session",
+                method: "get_cash_categories",
+                args: [[sessionId]],
+                kwargs: {},
+            });
+            this._cashCategoriesLoaded = true;
+            console.log("[CashCategory] Loaded categories:", this.cashCategories);
+        } catch (error) {
+            console.error("[CashCategory] Error loading categories:", error);
             this.cashCategories = [];
         }
     },
 
     async cashMove() {
-        if (this.config.use_cash_categories) {
+        console.log("[CashCategory] cashMove called, config:", this.config);
+        console.log("[CashCategory] use_cash_categories:", this.config?.use_cash_categories);
+
+        if (this.config && this.config.use_cash_categories) {
             await this.loadCashCategories();
 
             if (this.cashCategories && this.cashCategories.length > 0) {
+                console.log("[CashCategory] Showing custom popup");
                 this.hardwareProxy.openCashbox(_t("Cash in / out"));
                 const { confirmed } = await this.popup.add(CashCategoryPopup, {
                     title: _t("Cash In/Out"),
                     type: "in",
                 });
                 return confirmed;
+            } else {
+                console.log("[CashCategory] No categories, falling back to default");
             }
+        } else {
+            console.log("[CashCategory] Feature disabled, using default");
         }
         return super.cashMove(...arguments);
     },
