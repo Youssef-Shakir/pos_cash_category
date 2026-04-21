d6507eb61761ee939b46b1f45266291742c4a608
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-21 12:47:29 +0300

    add to 17

--- static/src/js/models.js
+++ static/src/js/models.js
@@ -4,27 +4,19 @@ import { patch } from "@web/core/utils/patch";
 import { CashCategoryPopup } from "@pos_cash_category/js/cash_category_popup";
 import { _t } from "@web/core/l10n/translation";
 import { jsonrpc } from "@web/core/network/rpc_service";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
-import { useService } from "@web/core/utils/hooks";
 import { Navbar } from "@point_of_sale/app/navbar/navbar";
+import { CashMovePopup } from "@point_of_sale/app/navbar/cash_move_popup/cash_move_popup";
 
 console.log("[CashCategory] Module loading...");
 
 patch(Navbar.prototype, {
-    setup() {
-        super.setup();
-        this._cashCategoryPos = usePos();
-        this._cashCategoryPopup = useService("popup");
-        console.log("[CashCategory] Navbar patched");
-    },
+    async onCashMoveButtonClick() {
+        console.log("[CashCategory] onCashMoveButtonClick called");
+        console.log("[CashCategory] use_cash_categories:", this.pos?.config?.use_cash_categories);
 
-    async onCashMoveButton() {
-        console.log("[CashCategory] onCashMoveButton called");
-        console.log("[CashCategory] use_cash_categories:", this._cashCategoryPos?.config?.use_cash_categories);
-
-        if (this._cashCategoryPos?.config?.use_cash_categories) {
+        if (this.pos?.config?.use_cash_categories) {
             try {
-                const sessionId = this._cashCategoryPos.pos_session?.id;
+                const sessionId = this.pos.pos_session?.id;
                 console.log("[CashCategory] Loading categories for session:", sessionId);
 
                 const categories = await jsonrpc("/web/dataset/call_kw/pos.session/get_cash_categories", {
@@ -37,8 +29,8 @@ patch(Navbar.prototype, {
                 console.log("[CashCategory] Categories loaded:", categories);
 
                 if (categories && categories.length > 0) {
-                    this._cashCategoryPos.cashCategories = categories;
-                    await this._cashCategoryPopup.add(CashCategoryPopup, {
+                    this.pos.cashCategories = categories;
+                    await this.popup.add(CashCategoryPopup, {
                         title: _t("Cash In/Out"),
                         type: "in",
                     });
@@ -49,8 +41,9 @@ patch(Navbar.prototype, {
             }
         }
 
-        // Fall back to default
-        return super.onCashMoveButton();
+        // Fall back to default - open cashbox and show default popup
+        this.hardwareProxy.openCashbox(_t("Cash in / out"));
+        this.popup.add(CashMovePopup);
     }
 });
 
